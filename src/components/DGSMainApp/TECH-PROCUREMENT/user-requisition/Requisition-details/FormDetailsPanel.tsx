6e151c8cf5c602ee3f99e159117f6009cd6d6011
Greateki <ekeneidiagbor@gmail.com>
2021-01-25 11:53:13 +0100

    designed the Form Details Panel in Requisition Details

--- src/components/DGSMainApp/TECH-PROCUREMENT/user-requisition/Requisition-details/FormDetailsPanel.tsx
+++ src/components/DGSMainApp/TECH-PROCUREMENT/user-requisition/Requisition-details/FormDetailsPanel.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { Row, Col } from 'antd';
 const FormDetailsPanel = () => {
 	return (
 		<div>
-			<h2>Form Details </h2>
+			<Row gutter={50}>
+				<Col span={20} offset={2}>
+					<Row>
+						<Col span={12}>
+							<p className='txt-small'>Request Title</p>
+							<strong>Personnel Protective Equipments (PPE) </strong>
+						</Col>
+						<Col span={12}>
+							<p className='txt-small'>Project</p>
+							<strong>Sapele Gas Plant</strong>
+						</Col>
+					</Row>
+					<Row gutter={50}>
+						<Col span={12}>
+							<p className='txt-small'>Client</p>
+							<strong>Seplat</strong>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		</div>
 	);
 };
