214f8b2ad1cd15853089a2a4f8a961a2ccfe0c22
Greateki <ekeneidiagbor@gmail.com>
2021-01-26 09:11:12 +0100

    added the elements of the mto form detail panel

--- src/components/DGSMainApp/TECH-PROCUREMENT/MTO/CreateMTO/MTODetails/MTOFormDetails.tsx
+++ src/components/DGSMainApp/TECH-PROCUREMENT/MTO/CreateMTO/MTODetails/MTOFormDetails.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Card, Row, Col } from 'antd';
 
 const MTOFormDetails = () => {
 	return (
 		<div>
-			<h2>MTO Form Details Panel</h2>
+			<Row gutter={50}>
+				<Col span={20} offset={2}>
+					<Row gutter={50}>
+						<Col span={24}>
+							<p className='txt-small'>Project Name </p>
+							<strong>
+								Procurement & Supply of Condensate Tank & 4 Actual Ball Valves &
+								Fitting for Pipeling Sysytem
+							</strong>
+						</Col>
+					</Row>
+					<Row gutter={50} className='my-2'>
+						<Col span={8}>
+							<p className='txt-small'>Project ID </p>
+							<strong>LKJBJ18965575</strong>
+						</Col>
+						<Col span={8}>
+							<p className='txt-small'>Client</p>
+							<strong>Nigerian Gas Company</strong>
+						</Col>
+						<Col span={8}>
+							<p className='txt-small'>Company</p>
+							<strong>DGS Integrated Products Limited </strong>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		</div>
 	);
 };
